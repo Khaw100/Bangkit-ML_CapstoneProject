57744eb00edc192a76c300223dc96fc3286be0a3
Khaw100 <rakhawiratama10@gmail.com>
2023-06-16 13:19:13 +0700

    upload testing file

--- API_Model/modelTesting.py
+++ API_Model/modelTesting.py
@@ -6,6 +6,8 @@ from modelFunction import *
 # Load Model
 mdl = tf.keras.models.load_model('model.h5')
 
+random_text = "He is but smart, but can't teach... Don't take the subject he teaches"
+
 def model_predict(random_text):
     random_text_sequence = tokenizer.texts_to_sequences([random_text])
     random_text_sequence = pad_sequences(random_text_sequence, maxlen = MAXLEN, padding=PADDING, truncating =TRUNCATING)
@@ -13,3 +15,6 @@ def model_predict(random_text):
     predicted_label = np.argmax(prediction)
     sentiment = "Positive" if predicted_label == 2 else "Neutral" if predicted_label == 1 else "Negative"
     return sentiment
+
+temp = model_predict(random_text)
+print(temp)
